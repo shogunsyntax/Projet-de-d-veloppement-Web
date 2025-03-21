68dda725a5bd0d52048a7315856c114078df0dc1
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-03-21 20:24:48 +0100

    Added Models Users

---
+++ EcommerceApplication/Models/Utilisateurs.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceApplication.Models
+{
+    public class Utilisateur
+    {   
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //! Identity correspond au Identity SQL
+        public int IdUtilisateur { get; set; }
+
+        [Required(ErrorMessage = "Le nom est obligatoire.")]
+        [StringLength(50, ErrorMessage = "Le nom ne peut pas dépasser 50 caractères.")]
+        public required string Nom { get; set; }
+
+        [Required(ErrorMessage = "Le prénom est obligatoire.")]
+        [StringLength(50, ErrorMessage = "Le prénom ne peut pas dépasser 50 caractères.")]
+        public required string Prenom { get; set; }
+
+        [Required(ErrorMessage = "L'email est obligatoire.")]
+        [EmailAddress(ErrorMessage = "L'email n'est pas valide.")]
+        public required string Email { get; set; }
+
+        [Required(ErrorMessage = "Le mot de passe est obligatoire.")]
+        [MinLength(8, ErrorMessage = "Le mot de passe doit contenir au moins 8 caractères.")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", 
+            ErrorMessage = "Le mot de passe doit contenir au moins une lettre, un chiffre et un caractère spécial.")]
+        public required string MotDePasse { get; set; }
+
+        [Required(ErrorMessage = "L'adresse est obligatoire.")]
+        [StringLength(255, ErrorMessage = "L'adresse ne peut pas dépasser 255 caractères.")]
+        public required string Adresse { get; set; }
+
+        [Required(ErrorMessage = "Le rôle est obligatoire.")]
+        [RegularExpression("^(Client|Admin)$", ErrorMessage = "Le rôle doit être 'Client' ou 'Admin'.")]
+        public required string Role { get; set; }
+    }
+}
--- EcommerceApplication/obj/Debug/net9.0/EcommerceApplication.AssemblyInfo.cs
+++ EcommerceApplication/obj/Debug/net9.0/EcommerceApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c37de1ec9133d63e3a29643773bedf0b199988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88195c9a2d0907b26bcdebe5e220d23fde1e44ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
