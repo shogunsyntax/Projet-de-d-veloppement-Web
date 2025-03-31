6e125e8d29377252bb61a15e26b61f6a8c5a6200
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-03-31 18:15:28 +0200

    update files

--- EcommerceApplication/Data/ECommerceDbContext.cs
+++ EcommerceApplication/Data/ECommerceDbContext.cs
@@ -20,6 +20,7 @@ namespace EcommerceApplication.Data
             modelBuilder.Entity<PanierProduit>().HasKey(pp => new { pp.PanierId, pp.ProduitId });
             modelBuilder.Entity<CommandeProduit>().HasKey(cp => new { cp.CommandeId, cp.ProduitId });
             modelBuilder.Entity<Utilisateur>().HasKey(uu => new{uu.IdUtilisateur,uu.IdRole});
+            modelBuilder.Entity<Utilisateur>().HasKey(ul=> new{ul.IdUtilisateur,ul.IdLocalite});
         }
     }
 }
