9a6713869028ea32147123a44140af4543600ed3
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-01 18:45:41 +0200

    update files

---
+++ EcommerceApplication/Migrations/20250401164228_update1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcommerceApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class update1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Commande_Panier_PanierId",
+                table: "Commande");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Commande_Panier_PanierId",
+                table: "Commande",
+                column: "PanierId",
+                principalTable: "Panier",
+                principalColumn: "PanierId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Commande_Panier_PanierId",
+                table: "Commande");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Commande_Panier_PanierId",
+                table: "Commande",
+                column: "PanierId",
+                principalTable: "Panier",
+                principalColumn: "PanierId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
