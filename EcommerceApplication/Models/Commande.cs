dba88332df5cb0171f62d0d7aa52b8106a1f09a4
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-04 18:12:56 +0200

    update files

--- EcommerceApplication/Models/Commande.cs
+++ EcommerceApplication/Models/Commande.cs
@@ -8,9 +8,9 @@ namespace EcommerceApplication.Models{
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]   
     public int CommandeId { get; set; }
 
-    public required int PanierId{get;set;}
+    public  int PanierId{get;set;}
     public int UtilisateurId { get; set; }
-    public required Utilisateur Utilisateur { get; set; }
+    public  Utilisateur? Utilisateur { get; set; }
     public required DateTime DateCommande { get; set; } 
     public required decimal Total { get; set; }
     public required string Statut { get; set; } 
