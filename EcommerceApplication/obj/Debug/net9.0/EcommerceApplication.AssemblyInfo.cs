63ffb8660a68d370048fd4036d4941c6ed236bc7
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-04 18:47:03 +0200

    update files

--- EcommerceApplication/obj/Debug/net9.0/EcommerceApplication.AssemblyInfo.cs
+++ EcommerceApplication/obj/Debug/net9.0/EcommerceApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b8f7740da25a27e31770c6cc5e841e2c491f3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+824707e06576ede7f3088ea2f6566ac67ee4a416")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
