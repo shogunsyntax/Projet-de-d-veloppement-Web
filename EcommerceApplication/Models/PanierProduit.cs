491dd74338ed8afbd2881741fabc8c21a19c8e1d
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-04 18:13:05 +0200

    update files

--- EcommerceApplication/Models/PanierProduit.cs
+++ EcommerceApplication/Models/PanierProduit.cs
@@ -7,7 +7,7 @@ namespace EcommerceApplication.Models{
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]    
     public int PanierId { get; set; }
-    public required Panier Panier { get; set; }
+    public Panier? Panier { get; set; }
     public required int ProduitId { get; set; }
     public required Produit Produit { get; set; }
     public required int Quantite { get; set; }
