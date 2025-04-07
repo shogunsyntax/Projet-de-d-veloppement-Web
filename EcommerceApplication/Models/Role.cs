07dae5976eedb207a4d06ad4014ca261dc90a305
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-07 20:07:27 +0200

    update files

--- EcommerceApplication/Models/Role.cs
+++ EcommerceApplication/Models/Role.cs
@@ -6,7 +6,7 @@ namespace EcommerceApplication.Models{
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public  int? RoleId { get; set; }
+    public  int? IdRole { get; set; }
     public required string IntituleRole{get;set;}
 
     public required string DetailRole{get;set;}
