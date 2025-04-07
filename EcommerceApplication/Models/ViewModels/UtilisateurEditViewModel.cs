88a5d5cb5dc2e52cd93c350bcacb0e3756fe8b8e
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-07 21:47:35 +0200

    update files

--- EcommerceApplication/Models/ViewModels/UtilisateurEditViewModel.cs
+++ EcommerceApplication/Models/ViewModels/UtilisateurEditViewModel.cs
@@ -5,22 +5,22 @@ namespace EcommerceApplication.Models.ViewModels{
 public class UtilisateurEditViewModel{ 
         public int IdUtilisateur { get; set; }
 
-        [Required(ErrorMessage = "Le rôle est obligatoire.")]
+        //[Required(ErrorMessage = "Le rôle est obligatoire.")]
         public int? IdRole { get; set; }
 
-        [Required(ErrorMessage = "La localité est obligatoire.")]
+        //[Required(ErrorMessage = "La localité est obligatoire.")]
         public int? IdLocalite { get; set; }
 
-        [Required, StringLength(50)]
+        //[Required, StringLength(50)]
         public string Nom { get; set; } = "";
 
-        [Required, StringLength(50)]
+        //[Required, StringLength(50)]
         public string Prenom { get; set; } = "";
 
-        [Required, StringLength(255)]
+        //[Required, StringLength(255)]
         public string Adresse { get; set; } = "";
 
-        [Required, EmailAddress]
+        //[Required, EmailAddress]
         public string Email { get; set; } = "";
 
         public string? MotDePasse { get; set; }
