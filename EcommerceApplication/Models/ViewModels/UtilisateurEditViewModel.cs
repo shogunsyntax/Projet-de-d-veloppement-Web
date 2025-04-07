429d5d89569cc17926bad4b65f1b22d23b4c3238
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-07 18:23:35 +0200

    update files

--- EcommerceApplication/Models/ViewModels/UtilisateurEditViewModel.cs
+++ EcommerceApplication/Models/ViewModels/UtilisateurEditViewModel.cs
@@ -2,31 +2,30 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.Rendering;
 namespace EcommerceApplication.Models.ViewModels{
   
-    public class UtilisateurEditViewModel{ 
+public class UtilisateurEditViewModel{ 
+        public int IdUtilisateur { get; set; }
 
-        public required int IdUtilisateur{get;set;}
-        [Required]
-        public required string Nom{get;set;}
+        [Required(ErrorMessage = "Le rôle est obligatoire.")]
+        public int? IdRole { get; set; }
 
-        [Required]
-        public required string Prenom{get;set;}
+        [Required(ErrorMessage = "La localité est obligatoire.")]
+        public int? IdLocalite { get; set; }
 
-        [Required,EmailAddress]
-        public required string Email{get;set;}
+        [Required, StringLength(50)]
+        public string Nom { get; set; } = "";
 
-        public string? MotDePasse{get;set;} //! Pas obligatoire
-      
-        [Required]
-        public required string Adresse{get;set;}
+        [Required, StringLength(50)]
+        public string Prenom { get; set; } = "";
 
-       [Required]
-        public  int? IdRole{get;set;}
+        [Required, StringLength(255)]
+        public string Adresse { get; set; } = "";
 
+        [Required, EmailAddress]
+        public string Email { get; set; } = "";
 
-        public  int? IdLocalite{get; set;}
+        public string? MotDePasse { get; set; }
 
-        public List<SelectListItem> Localites = new List<SelectListItem>();
-
-        public List<SelectListItem> Roles = new List<SelectListItem>();
-
+        // On ajoute les listes directement dans le ViewModel
+        public List<SelectListItem> Roles { get; set; } = new();
+        public List<SelectListItem> Localites { get; set; } = new();
 }}
