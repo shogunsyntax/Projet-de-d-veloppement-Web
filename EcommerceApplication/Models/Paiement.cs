3bf278e190fdae31a6cda377e11337d9427fff23
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-07 20:07:11 +0200

    update files

--- EcommerceApplication/Models/Paiement.cs
+++ EcommerceApplication/Models/Paiement.cs
@@ -4,8 +4,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace EcommerceApplication.Models{
     public class Paiement{
         
-    public required int Id { get; set; }
-    public required int CommandeId { get; set; }
+    public required int IdPaiement { get; set; }
+    public required int IdCommande { get; set; }
     public required Commande Commande { get; set; }
     public decimal Montant { get; set; }
     public required string Statut { get; set; } 
