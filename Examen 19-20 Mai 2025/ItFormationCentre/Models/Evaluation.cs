123b71d33f418fd9f225270de089c6bb65557f71
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-15 19:35:06 +0200

    refactor: update UtilisateurEditViewModel structure and validation attributes

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/Evaluation.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/Evaluation.cs
@@ -9,17 +9,21 @@ namespace ItFormationCentre.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdEvaluation { get; set; }
 
-        [Range(0, 20)]
-        public float Note { get; set; }
+    [ForeignKey("Formateur")]
+    public int IdUtilisateurFormateur { get; set; }
 
-        public string? Commentaire { get; set; }
+    [ForeignKey("Stagiaire")]
+    public int IdUtilisateurStagiaire { get; set; }
 
-        public DateTime DateEvaluation { get; set; }
+    [ForeignKey("SessionFormation")]
+    public int IdSessionFormation { get; set; }
+    [Range(0,20)]
+    public decimal Note { get; set; }
+    public string Commentaire { get; set; }
+    public DateTime DateEvaluation { get; set; }
 
-        public int IdUtilisateur { get; set; }
-        public Utilisateur? Utilisateur { get; set; }
-
-        public int IdSessionFormation { get; set; }
-        public SessionFormation? SessionFormation { get; set; }
+    public  Formateur Formateur { get; set; }
+    public Stagiaire Stagiaire { get; set; }
+    public  SessionFormation SessionFormation { get; set; }
     }
 }
