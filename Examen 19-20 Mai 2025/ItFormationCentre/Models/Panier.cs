76dfe9330a86ebd42440c226e6e51977650aa77d
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-15 19:35:28 +0200

    refactor: update UtilisateurEditViewModel structure and validation attributes

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/Panier.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/Panier.cs
@@ -5,33 +5,25 @@ namespace ItFormationCentre.Models
 {
     public class Panier
     {
-        [Key]
+      [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdPanier { get; set; }
 
         public DateTime DateCreation { get; set; }
-
         public DateTime? DateValidation { get; set; }
-
         public string? Statut { get; set; }
-
         public decimal Total { get; set; }
-
         public string? MethodePaiement { get; set; }
-
         public string? CodePromo { get; set; }
-
         public decimal? Reduction { get; set; }
-
         public decimal Tva { get; set; }
-
         public decimal NetAPayer { get; set; }
 
         public int IdUtilisateur { get; set; }
         public Utilisateur? Utilisateur { get; set; }
 
-        //public ICollection<Inscription> Inscriptions { get; set; }
+        public ICollection<Ajouter> Ajouts { get; set; } 
 
-        //public ICollection<Paiement> Paiements { get; set; }
+     
     }
 }
