bf597cb19a16f6d03ad92aba12d2332543035166
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-15 19:35:03 +0200

    refactor: update UtilisateurEditViewModel structure and validation attributes

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/Ajouter.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/Ajouter.cs
@@ -1,18 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ItFormationCentre.Models
 {
-    public class Ajouter
+
+        public class Ajouter
     {
-       [Key]
-       [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdAjouter { get; set; } // Clé primaire
 
-        [Required]
-        public int IdPanier { get; set; }
-        public Panier Panier { get; set; }
+        [ForeignKey("Panier")]
+        public int IdPanier { get; set; } // Clé étrangère vers Panier
 
-        [Required]
-        public int IdFormation { get; set; }
-        public Formation Formation { get; set; }
+        [ForeignKey("Formation")]
+        public int IdFormation { get; set; } // Clé étrangère vers Formation
+
+        public Panier Panier { get; set; } // Navigation vers Panier
+        public Formation Formation { get; set; } // Navigation vers Formation
     }
 }
