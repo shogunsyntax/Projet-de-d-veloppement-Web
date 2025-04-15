2e7dcbacd0c72ed16ad9b4ef0ef4c9863d412945
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-15 19:35:50 +0200

    refactor: update UtilisateurEditViewModel structure and validation attributes

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/Role.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/Role.cs
@@ -6,18 +6,14 @@ namespace ItFormationCentre.Models
 {
     public class Role
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int IdRole { get; set; }
+      [Key]
+    public int IdRole { get; set; }
 
-        [Required(ErrorMessage = "L'intitulé du rôle est obligatoire.")]
-        [StringLength(100)]
-        public required string IntituleRole { get; set; }
+    [Required]
+    [StringLength(100)]
+    public string IntituleRole { get; set; }
 
-        // Liste des utilisateurs ayant ce rôle
-        public ICollection<Utilisateur> Utilisateurs { get; set; }
-
-        // Liste des permissions associées au rôle
-        public ICollection<Permission> Permissions { get; set; }
+    public  ICollection<Utilisateur> Utilisateurs { get; set; }
+    public  ICollection<Detenir> Permissions { get; set; }
     }
 }
