a81bee5dbe2d43aac182250ac0093dc2db34a619
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-04-15 19:33:12 +0200

    refactor: update UtilisateurEditViewModel structure and validation attributes

--- EcommerceApplication/Models/ViewModels/UtilisateurEditViewModel.cs
+++ EcommerceApplication/Models/ViewModels/UtilisateurEditViewModel.cs
@@ -1,31 +1,45 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.Rendering;
-namespace EcommerceApplication.Models.ViewModels{
-  
-public class UtilisateurEditViewModel{ 
+
+namespace ItFormationCentre.ViewModels
+{
+    public class UtilisateurViewModel
+    {
         public int IdUtilisateur { get; set; }
 
-        //[Required(ErrorMessage = "Le rôle est obligatoire.")]
-        public int? IdRole { get; set; }
+        public int IdRole { get; set; }
 
-        //[Required(ErrorMessage = "La localité est obligatoire.")]
-        public int? IdLocalite { get; set; }
+        public int IdLocalite { get; set; }
 
-        //[Required, StringLength(50)]
-        public string Nom { get; set; } = "";
+        [Required(ErrorMessage = "Le nom est requis.")]
+        [StringLength(100, ErrorMessage = "Le nom ne peut pas dépasser 100 caractères.")]
+        public string Nom { get; set; }
 
-        //[Required, StringLength(50)]
-        public string Prenom { get; set; } = "";
+        [Required(ErrorMessage = "Le prénom est requis.")]
+        [StringLength(100, ErrorMessage = "Le prénom ne peut pas dépasser 100 caractères.")]
+        public string Prenom { get; set; }
 
-        //[Required, StringLength(255)]
-        public string Adresse { get; set; } = "";
+        [Required(ErrorMessage = "Le nom d'utilisateur est requis.")]
+        [StringLength(50, ErrorMessage = "Le nom d'utilisateur ne peut pas dépasser 50 caractères.")]
+        public string NomUtilisateur { get; set; }
 
-        //[Required, EmailAddress]
-        public string Email { get; set; } = "";
+        [Required(ErrorMessage = "L'email est requis.")]
+        [EmailAddress(ErrorMessage = "L'email n'est pas valide.")]
+        public string Email { get; set; }
 
-        public string? MotDePasse { get; set; }
+        [Required(ErrorMessage = "Le mot de passe est requis.")]
+        [StringLength(100, ErrorMessage = "Le mot de passe ne peut pas dépasser 100 caractères.")]
+        public string MotDePasse { get; set; }
 
-        // On ajoute les listes directement dans le ViewModel
-        public List<SelectListItem> Roles { get; set; } = new();
-        public List<SelectListItem> Localites { get; set; } = new();
-}}
+        [Phone(ErrorMessage = "Le numéro de téléphone n'est pas valide.")]
+        public string Telephone { get; set; }
+
+        public string Diplome { get; set; }
+
+        public int IdAdresse { get; set; }
+
+        // Propriétés supplémentaires pour les listes déroulantes
+        public IEnumerable<Role> Roles { get; set; }
+        public IEnumerable<Localite> Localites { get; set; }
+    }
+}
