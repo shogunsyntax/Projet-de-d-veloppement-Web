b5369a7c9ae3dfbb802b67536cc8dcd01824008d
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:41:40 +0200

    Update Files CRUD Fonctionnel

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/Panier.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/Panier.cs
@@ -1,29 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace ItFormationCentre.Models
 {
     public class Panier
     {
-      [Key]
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdPanier { get; set; }
 
-        public DateTime DateCreation { get; set; }
-        public DateTime? DateValidation { get; set; }
-        public string? Statut { get; set; }
-        public decimal Total { get; set; }
-        public string? MethodePaiement { get; set; }
-        public string? CodePromo { get; set; }
-        public decimal? Reduction { get; set; }
-        public decimal Tva { get; set; }
-        public decimal NetAPayer { get; set; }
-
+        [ForeignKey("Utilisateur")]
         public int IdUtilisateur { get; set; }
+
+        // Propriété de navigation
         public Utilisateur? Utilisateur { get; set; }
 
-        public ICollection<Ajouter> Ajouts { get; set; } 
+        // Propriété de navigation pour les ajouts au panier
+        public ICollection<Ajouter> Ajouts { get; set; } = new List<Ajouter>();
 
-     
+        // Autres propriétés du panier
+        public DateTime DateCreation { get; set; }
+        public decimal Total { get; set; }
     }
 }
