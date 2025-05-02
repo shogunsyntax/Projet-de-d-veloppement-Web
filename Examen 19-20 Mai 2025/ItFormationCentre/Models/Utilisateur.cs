dd56dab9b1b47337a6e54fef2939d17809703bef
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:41:47 +0200

    Update Files CRUD Fonctionnel

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/Utilisateur.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/Utilisateur.cs
@@ -1,60 +1,63 @@
 using System;
-
 using System.Collections.Generic;
-
 using System.ComponentModel.DataAnnotations;
-
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ItFormationCentre.Models 
+namespace ItFormationCentre.Models
 {
-public class Utilisateur{
-  [Key]
-    public int IdUtilisateur { get; set; }
+    public abstract class Utilisateur
+    {
+        [Key]
+        public int IdUtilisateur { get; set; }
 
-    [ForeignKey("Role")]
-    public int IdRole { get; set; }
+        [ForeignKey("Role")]
+        [Required] // Rendre obligatoire la relation avec Role
+        public int IdRole { get; set; }
 
-    [ForeignKey("Localite")]
-    public int IdLocalite { get; set; }
+        [ForeignKey("Localite")]
+        [Required] // Rendre obligatoire la relation avec Localite
+        public int IdLocalite { get; set; }
 
-    [Required]
-    [StringLength(100)]
-    public string Nom { get; set; }
+        [Required] // Rendre obligatoire
+        [StringLength(100)]
+        public string Nom { get; set; }
 
-    [Required]
-    [StringLength(100)]
-    public string Prenom { get; set; }
+        [Required] // Rendre obligatoire
+        [StringLength(100)]
+        public string Prenom { get; set; }
 
-    [Required]
-    [StringLength(50)]
-    public string NomUtilisateur { get; set; }
+        [Required] // Rendre obligatoire
+        [StringLength(50)]
+        public string NomUtilisateur { get; set; }
 
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; }
+        [Required] // Rendre obligatoire
+        [EmailAddress] 
+        public string Email { get; set; }
 
-    [Required]
-    [StringLength(100)]
-    public string MotDePasse { get; set; }
+        [Required] // Rendre obligatoire
+        [StringLength(100)]
+        public string MotDePasse { get; set; }
 
-    [Phone]
-    public string Telephone { get; set; }
+        [Phone]
+        public string Telephone { get; set; }
 
-    public string Diplome { get; set; }
+        public string Diplome { get; set; }
 
-    [StringLength(250)]
-    public int IdAdresse { get; set; }
-    public Adresse? Adresse { get; set; }
+        [Required] // Rendre obligatoire
+        public int IdAdresse { get; set; }
 
+        // Propriétés de navigation
+        [Required] // Rendre obligatoire la relation avec Adresse
+        public Adresse Adresse { get; set; }
 
-    public  Role? Role { get; set; }
-    public  Localite? Localite { get; set; }
+        [Required] // Rendre obligatoire la relation avec Role
+        public Role Role { get; set; }
 
-  public ICollection<Evaluation> EvaluationsStagiaire { get; set; }
-  public ICollection<Evaluation> EvaluationsFormateur { get; set; }
+        [Required] // Rendre obligatoire la relation avec Localite
+        public Localite Localite { get; set; }
 
-  //public ICollection<Adresse> Adresses { get; set; }
-
-}
+        // Collections pour les évaluations
+        public ICollection<Evaluation> EvaluationsStagiaire { get; set; } = new List<Evaluation>();
+        public ICollection<Evaluation> EvaluationsFormateur { get; set; } = new List<Evaluation>();
+    }
 }
