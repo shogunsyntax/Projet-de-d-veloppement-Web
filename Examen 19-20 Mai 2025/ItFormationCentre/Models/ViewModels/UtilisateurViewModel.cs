58312c3453869f31092d6edc2cb92b641b788a0c
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:41:58 +0200

    Update Files CRUD Fonctionnel

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/ViewModels/UtilisateurViewModel.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/ViewModels/UtilisateurViewModel.cs
@@ -8,20 +8,13 @@ namespace ItFormationCentre.Models.ViewModels
     {
         public int IdUtilisateur { get; set; }
 
-        public int IdRole { get; set; }
-
-        public int IdLocalite { get; set; }
-
         [Required(ErrorMessage = "Le nom est requis.")]
-        [StringLength(100, ErrorMessage = "Le nom ne peut pas dépasser 100 caractères.")]
         public string Nom { get; set; }
 
         [Required(ErrorMessage = "Le prénom est requis.")]
-        [StringLength(100, ErrorMessage = "Le prénom ne peut pas dépasser 100 caractères.")]
         public string Prenom { get; set; }
 
         [Required(ErrorMessage = "Le nom d'utilisateur est requis.")]
-        [StringLength(50, ErrorMessage = "Le nom d'utilisateur ne peut pas dépasser 50 caractères.")]
         public string NomUtilisateur { get; set; }
 
         [Required(ErrorMessage = "L'email est requis.")]
@@ -29,18 +22,41 @@ namespace ItFormationCentre.Models.ViewModels
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Le mot de passe est requis.")]
-        [StringLength(100, ErrorMessage = "Le mot de passe ne peut pas dépasser 100 caractères.")]
+        [DataType(DataType.Password)]
         public string MotDePasse { get; set; }
 
-        [Phone(ErrorMessage = "Le numéro de téléphone n'est pas valide.")]
+        [Required(ErrorMessage = "Le numéro de téléphone est requis.")]
         public string Telephone { get; set; }
 
         public string Diplome { get; set; }
 
+        [Required(ErrorMessage = "Le rôle est requis.")]
+        public int IdRole { get; set; }
+
+        [Required(ErrorMessage = "La localité est requise.")]
+        public int IdLocalite { get; set; }
+
         public int IdAdresse { get; set; }
 
-        // Propriétés supplémentaires pour les listes déroulantes
-        public IEnumerable<Role> Roles { get; set; } = new List<Role>();
-        public IEnumerable<Localite> Localites { get; set; } = new List<Localite>();
+        // Ajout des propriétés liées à l'adresse
+        [Required(ErrorMessage = "La rue est requise.")]
+        public string Rue { get; set; } // Rue de l'adresse
+
+        [Required(ErrorMessage = "La ville est requise.")]
+        public string Ville { get; set; } // Ville
+
+        [Required(ErrorMessage = "Le pays est requis.")]
+        public string Pays { get; set; } // Pays
+
+         // Ajout des champs spécifiques
+        // Champ pour stagiaire
+        public string CarteEtudiant { get; set; }
+
+        // Champ pour formateur
+        public string Specialite { get; set; }
+
+        // Propriétés pour les listes déroulantes
+        public List<Role> Roles { get; set; } = new List<Role>();
+        public List<Localite> Localites { get; set; } = new List<Localite>();
     }
 }
