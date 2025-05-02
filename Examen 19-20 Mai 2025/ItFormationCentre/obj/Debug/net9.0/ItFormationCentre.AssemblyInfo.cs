6620d712dd97d3e7845ac830acfcded33a7678cc
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:42:18 +0200

    Update Files CRUD Fonctionnel

--- Examen 19-20 Mai 2025/ItFormationCentre/obj/Debug/net9.0/ItFormationCentre.AssemblyInfo.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/obj/Debug/net9.0/ItFormationCentre.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ItFormationCentre")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472067e264097b6abbb399fa2ceb755ed1c29165")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d11839bcffc32381d960e668585cb5c09f82ca1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ItFormationCentre")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ItFormationCentre")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
