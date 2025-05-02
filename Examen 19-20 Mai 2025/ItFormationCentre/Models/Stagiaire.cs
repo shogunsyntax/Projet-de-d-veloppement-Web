10386b60889cc73237081488bc1e1e7ab2e3e8a5
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:41:44 +0200

    Update Files CRUD Fonctionnel

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/Stagiaire.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/Stagiaire.cs
@@ -1,14 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
+
 namespace ItFormationCentre.Models
 {
     public class Stagiaire : Utilisateur
     {
         [Required]
-        public string CarteEtudiant { get; set; }
+        public string? CarteEtudiant { get; set; }
 
-        public ICollection<Evaluation> Evaluations { get; set; }
+        public ICollection<Evaluation> Evaluations { get; set; } = new List<Evaluation>();
 
 
        
