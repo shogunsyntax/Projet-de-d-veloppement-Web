1d4ef958e5ba959706c770fa88e1b92d27c0d0e9
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:41:31 +0200

    Update Files CRUD Fonctionnel

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/Formateur.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/Formateur.cs
@@ -1,19 +1,16 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
 
 namespace ItFormationCentre.Models
 {
     public class Formateur : Utilisateur
     {
-        [Required]
-        public string Specialite { get; set; }
+        public string? Specialite { get; set; }
 
-        [Range(0, 50)]
-        public int Experience { get; set; }
+        // Sessions de formation que ce formateur anime
+        public ICollection<SessionFormation> Sessions { get; set; } = new List<SessionFormation>();
 
-        public ICollection<SessionFormation> SessionsFormation { get; set; }
-
-        public ICollection<Evaluation> Evaluations { get; set; }
-
+        // Évaluations que ce formateur a données
+        public ICollection<Evaluation> Evaluations { get; set; } = new List<Evaluation>();
+        
     }
 }
