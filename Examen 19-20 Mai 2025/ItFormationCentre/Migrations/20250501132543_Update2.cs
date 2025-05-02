3ea256bc882af76700ff0cc5cc4538b541cdba9e
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:40:55 +0200

    Update Files CRUD Fonctionnel

---
+++ Examen 19-20 Mai 2025/ItFormationCentre/Migrations/20250501132543_Update2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ItFormationCentre.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MotDePasse",
+                table: "Utilisateurs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "MotDePasse",
+                table: "Utilisateurs",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
