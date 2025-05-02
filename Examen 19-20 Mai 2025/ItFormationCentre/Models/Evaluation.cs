f25d1fd17581c722fdbdbb98bb61fa00ecbdb227
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:41:25 +0200

    Update Files CRUD Fonctionnel

--- Examen 19-20 Mai 2025/ItFormationCentre/Models/Evaluation.cs
+++ Examen 19-20 Mai 2025/ItFormationCentre/Models/Evaluation.cs
@@ -9,21 +9,23 @@ namespace ItFormationCentre.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdEvaluation { get; set; }
 
-    [ForeignKey("Formateur")]
-    public int IdUtilisateurFormateur { get; set; }
+        public int IdUtilisateurFormateur { get; set; }
+        public int IdUtilisateurStagiaire { get; set; }
+        public int IdSessionFormation { get; set; }
 
-    [ForeignKey("Stagiaire")]
-    public int IdUtilisateurStagiaire { get; set; }
+        [Range(0, 20)]
+        public decimal Note { get; set; }
 
-    [ForeignKey("SessionFormation")]
-    public int IdSessionFormation { get; set; }
-    [Range(0,20)]
-    public decimal Note { get; set; }
-    public string Commentaire { get; set; }
-    public DateTime DateEvaluation { get; set; }
+        public string? Commentaire { get; set; }
+        public DateTime DateEvaluation { get; set; }
 
-    public  Formateur Formateur { get; set; }
-    public Stagiaire Stagiaire { get; set; }
-    public  SessionFormation SessionFormation { get; set; }
+        [ForeignKey("IdUtilisateurFormateur")]
+        public Formateur? Formateur { get; set; }
+
+        [ForeignKey("IdUtilisateurStagiaire")]
+        public Stagiaire? Stagiaire { get; set; }
+
+        [ForeignKey("IdSessionFormation")]
+        public SessionFormation? SessionFormation { get; set; }
     }
 }
