a073d5a478f4496b337fb35b2f3488c168b5740c
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:39:28 +0200

    Update Files CRUD Fonctionnel

--- Examen 19-20 Mai 2025/ItFormationCentre/tailwind.config.js
+++ Examen 19-20 Mai 2025/ItFormationCentre/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './Pages/**/*.cshtml',
-    './Views/**/*.cshtml'
+    "./Views/**/*.cshtml",
+    "./Pages/**/*.cshtml",
+    "./wwwroot/**/*.html",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
