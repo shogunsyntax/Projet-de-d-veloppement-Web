9081811c9c14c846c97e42e86d6068df33116581
Alexandre Beugnies <beugniesalexandre@outlook.com>
2025-05-02 12:40:32 +0200

    Update Files CRUD Fonctionnel

---
+++ Examen 19-20 Mai 2025/ItFormationCentre/Migrations/20250430191734_UpdateAdresse.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ItFormationCentre.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAdresse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Ville",
+                table: "Adresses",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Ville",
+                table: "Adresses");
+        }
+    }
+}
